6b6b91d00add914727de339dd639227824aa4044
David González <dev.dvdgzlez@gmail.com>
2019-06-24 20:12:36 +0200

    Adding __tests__ folder and placing selfieList code tests

---
+++ NamecheapTest/ClientApp/src/__tests__/SelfieList.test.js
@@ -0,0 +1,70 @@
+﻿import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import SelfieList from "../components/Selfie/SelfieList";
+
+configure({ adapter: new Adapter() });
+
+describe('<SelfieList />', () => {
+    it('should render error message if user list is not set', () => {
+        let props = {
+            users: []
+        };
+        let wrapper = shallow(<SelfieList {...props} />);
+        expect(wrapper.contains(<div className="ErrorMessage">There are no users stored</div>)).toEqual(true);
+    });
+
+    it('should render table with users name and email', () => {
+        let props = {
+            users: [
+                {
+                    name: "John Doe",
+                    email: "john.doe@gmail.com"
+                }
+            ],
+            clicked: () => {
+
+            }
+        };
+        let wrapper = shallow((<SelfieList {...props} />));
+        expect(wrapper.find('td')).toHaveLength(2);
+        expect(wrapper.find('td').at(0).text()).toEqual("john.doe@gmail.com");
+        expect(wrapper.find('td').at(1).text()).toEqual("John Doe");
+        props = {
+            users: [
+                {
+                    name: "John Doe",
+                    email: "john.doe@gmail.com"
+                },
+                {
+                    name: "John Doe",
+                    email: "john.doe@gmail.com"
+                }
+            ],
+            clicked: () => {
+
+            }
+        };
+
+        wrapper = shallow(<SelfieList {...props} />);
+        expect(wrapper.find('td')).toHaveLength(4);
+        expect(wrapper.find('td').at(0).text()).toEqual("john.doe@gmail.com");
+        expect(wrapper.find('td').at(1).text()).toEqual("John Doe");
+        expect(wrapper.find('td').at(2).text()).toEqual("john.doe@gmail.com");
+        expect(wrapper.find('td').at(3).text()).toEqual("John Doe");
+    });
+
+    it("should throw an error exception on clicked event not defined", () => {
+        let props = {
+            users: [
+                {
+                    name: "John Doe",
+                    email: "john.doe@gmail.com"
+                }
+            ]
+        };
+        expect(() => shallow((<SelfieList {...props} />))).toThrow("'clicked' event not defined");
+    });
+});
--- NamecheapTest/ClientApp/src/components/Selfie/SelfieList.js
+++ NamecheapTest/ClientApp/src/components/Selfie/SelfieList.js
@@ -1,6 +1,12 @@
 ﻿import React from "react";
 
 const list = (props) => {
+    if (!props.users || props.users.length === 0) {
+        return <div className="ErrorMessage">There are no users stored</div>;
+    }
+    if (!props.clicked) {
+        throw "'clicked' event not defined";
+    }
     const tableRow = props.users.map(user => (
         <tr key={`user${props.users.indexOf(user)}`}>
             <td><label onClick={() => props.clicked(user)}>{user.email}</label></td>
